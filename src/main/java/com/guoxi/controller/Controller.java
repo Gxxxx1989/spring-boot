86deb81d0d12e391b695bcd3477ed6df5d22d681
guoxi <guoxi@risingstarinfo.com>
2018-12-27 15:28:39 +0800

    spring-boot

--- src/main/java/com/guoxi/controller/Controller.java
+++ src/main/java/com/guoxi/controller/Controller.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
-    @RequestMapping("/hello")
+    @RequestMapping("/hello-guoxi")
     public String method(){
         return "hello world";
     }
