820fb105b010e689187b65cbb2b76c7f9f80a6cc
guoxi <guoxi@risingstarinfo.com>
2018-12-27 12:58:47 +0800

    springboot hello world

--- src/main/java/App.java
+++
@@ -1,7 +0,0 @@
-import org.springframework.boot.SpringApplication;
-
-public class App {
-    public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
-    }
-}
---
+++ src/main/java/main/App.java
@@ -0,0 +1,13 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
