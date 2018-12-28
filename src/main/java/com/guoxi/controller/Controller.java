23f53ee42d0bd6787b2a25afb4e91ce43d208ec5
guoxi <guoxi@risingstarinfo.com>
2018-12-28 11:18:00 +0800

    ä¿®æ¹ä»£ç 

--- src/main/java/com/guoxi/controller/Controller.java
+++ src/main/java/com/guoxi/controller/Controller.java
@@ -2,10 +2,21 @@ package com.guoxi.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Date;
+import java.util.Map;
+
 @RestController
 public class Controller {
     @RequestMapping("/hello-guoxi")
-    public String method(){
+    public String method() {
         return "hello world";
+    }
+
+
+    @RequestMapping(value = {"/", "/index"})
+    public String index(Map<String, Object> model) {
+        model.put("time", new Date());
+        model.put("message", "hello");
+        return "index";
     }
 }
---
+++ src/main/java/com/guoxi/module/dao/Analysis.java
@@ -0,0 +1,43 @@
+package com.guoxi.module.dao;
+
+public class Analysis {
+    private Integer id;
+
+    private String name;
+
+    private String age;
+
+    private String sex;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age == null ? null : age.trim();
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex == null ? null : sex.trim();
+    }
+}
