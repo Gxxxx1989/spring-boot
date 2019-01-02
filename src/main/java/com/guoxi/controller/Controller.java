6e794e05268e53dc7a107bbe670f00d8d387cb4d
guoxi <guoxi@risingstarinfo.com>
2019-01-02 18:07:44 +0800

    modify by gxï

--- src/main/java/com/guoxi/controller/Controller.java
+++ src/main/java/com/guoxi/controller/Controller.java
@@ -1,22 +1,25 @@
 package com.guoxi.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/web/")
 public class Controller {
-    @RequestMapping("/hello-guoxi")
-    public String method() {
-        return "hello world";
+
+    private static final Logger logger = LoggerFactory.getLogger(Controller.class);
+//    @Autowired
+//    private AdminUserService adminUserService;
+
+    @RequestMapping("login")
+    public String helloHtml(HashMap<String, Object> map) {
+        return "/hello";
     }
 
-
-    @RequestMapping(value = {"/", "/index"})
-    public String index(Map<String, Object> model) {
-        model.put("time", new Date());
-        model.put("message", "hello");
-        return "index";
-    }
 }
