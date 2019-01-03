a62edac013976f03f095c42a0025c178525e7fe0
guoxi <guoxi@risingstarinfo.com>
2019-01-03 09:55:50 +0800

    add page index.html

--- src/main/java/com/guoxi/controller/Controller.java
+++ src/main/java/com/guoxi/controller/Controller.java
@@ -1,25 +1,12 @@
 package com.guoxi.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+@org.springframework.stereotype.Controller
 
-@RestController
-@RequestMapping("/web/")
 public class Controller {
-
-    private static final Logger logger = LoggerFactory.getLogger(Controller.class);
-//    @Autowired
-//    private AdminUserService adminUserService;
-
-    @RequestMapping("login")
-    public String helloHtml(HashMap<String, Object> map) {
-        return "/hello";
+    @RequestMapping("/login")
+    public String testHtml() {
+        return "index.html";
     }
-
 }
