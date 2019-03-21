176fcaf6dd6a13d4a348f95da49fbfbc4ebdae4d
guoxi <guoxi@risingstarinfo.com>
2019-03-21 16:12:03 +0800

    修改mybatis配置

--- src/main/java/com/guoxi/controller/Controller.java
+++ src/main/java/com/guoxi/controller/Controller.java
@@ -1,23 +1,30 @@
 package com.guoxi.controller;
 
 import com.guoxi.module.dao.UserInfo;
+import com.guoxi.module.dao.UserInfoMapper;
 import com.guoxi.service.UserService;
-import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@Api(value="controller",tags={"用户操作接口"})
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @org.springframework.stereotype.Controller
 public class Controller {
 
     @Autowired
     UserService userService;
 
-//    @RequestMapping(value = {"/", "/login"})
-//    public String testHtml() {
-//        return "index.html";
-//    }
+    @RequestMapping(value = {"/", "/login"})
+    @ResponseBody
+    public String testHtml() {
+        return "index.html";
+    }
 //
 //    @RequestMapping(value = "/user")
 //    public String user(HttpServletRequest request) {
@@ -35,14 +42,14 @@ public class Controller {
     @ApiOperation(value = "新增用户" , notes = "新增用户信息")
     @PostMapping(value = "/insert")
     @ResponseBody
-    public int insertUser(@RequestBody UserInfo userInfo) {
+    public int insertUser(UserInfo userInfo) {
         return userService.insertUser(userInfo);
     }
 
     @ApiOperation(value = "查询用户" , notes = "查询用户信息")
     @PostMapping(value = "/getUser")
     @ResponseBody
-    public  UserInfo getUser(int id) {
+    public UserInfo getUser(int  id) {
         return userService.getUserInfo(id);
     }
 
